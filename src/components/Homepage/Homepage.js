7e3436cc49a917a59ffb7d5ca0c0dc4cc0d785a8
Chris Lee <leechr2012@gmail.com>
2023-03-26 19:10:44 -0700

    portfolio changes

---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import '../../pages/style.css';
+import { Container, Row, Col } from 'react-bootstrap'
+import Text from '../Homepage/Text'
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home() {
+  return (
+    <div className='homepagebackground'>
+      <Container>
+        <Row>
+          <Col md={7}>
+            <h2 className='headtext'>Hello <span className='wave'>👋 </span></h2>
+            <h2 className='nametext'>I am Christian Lee</h2>
+            <span></span>
+            <Text />
+            <button onClick={() => {
+              window.open("https://www.linkedin.com/in/christian-lee-495550126/");
+            }}
+              className='socailmediabtn'><FaLinkedinIn className='icon' /></button>
+            <button onClick={() => {
+              window.open("https://gitlab.com/chris_lee253");
+            }}
+              className='socailmediabtn'><AiFillGithub className='icon' /></button>
+          </Col>
+
+          <Col md={5}>
+            <div className="imagedeveloper">
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+
+
+  )
+}
+
+export default Home
