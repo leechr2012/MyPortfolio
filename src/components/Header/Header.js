df3195ea543a085ea9337f7391b8211994c82d9c
Chris Lee <leechr2012@gmail.com>
2023-04-04 11:33:42 -0700

    made changes to resume

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -70,7 +70,7 @@ function Header() {
 
           <Button
             onClick={() => {
-              window.open("https://drive.google.com/file/d/1QGMS5QpVXz1azYNsedqbA3wOFBFqA3VV/view?usp=sharing");
+              window.open("https://drive.google.com/file/d/1WzIAzm5bIaukiNUHi9YOGnV7TJ0zToY1/view?usp=sharing");
             }}
             className='resumebtn'><span>Resume</span>
           </Button>
